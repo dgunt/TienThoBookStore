50f7e3040a133fa780d94a54f4a1aa9af1efb047
dgunt <vuducminhvn2003@gmail.com>
2025-04-17 20:12:35 +0700

    Thêm folder DTO, Mapping,Service

---
+++ TienThoBookStore.Application/Services/Implementations/BookService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TienThoBookStore.Application.Services.Interfaces;
+
+namespace TienThoBookStore.Application.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+
+    }
+}
